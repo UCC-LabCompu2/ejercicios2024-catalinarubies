6498ad951c33e1429b65e69b364f8c770850ae33
catalinarubies <2211430@ucc.edu.ar>
2024-06-08 19:09:07 -0300

    se realizo ejercicio grados a radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -32,3 +32,16 @@ function cambiarunidades (id, valor){
     }
 
 }
+
+function convertirGR(id){
+    var grad, rad;
+    if(id=="grados"){
+        grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+    }else if(id=="radianes"){
+        rad = document.getElementById("radianes").value;
+        grad = (rad*180)/Math.PI;
+    }
+    document.getElementById("grados").value= grad;
+    document.getElementById("radianes").value= rad;
+}
