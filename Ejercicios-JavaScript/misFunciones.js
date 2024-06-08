062bf8de2e037df3951751dfe85a427536ebc84b
catalinarubies <2211430@ucc.edu.ar>
2024-06-08 20:13:01 -0300

    se realizo ejercicio de operaciones matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -58,3 +58,40 @@ function mostrar_ocultar(valorMO){
         document.getElementById("divMO").style.display = 'none';
     }
 }
+
+function calcularsuma(){
+    var num1, num2;
+
+    num1=Number(document.getElementsByName("sum_num1")[0].value);
+    num2=Number(document.getElementsByName("sum_num2")[0].value);
+
+    document.getElementsByName("sum_total")[0].value = num1 + num2;
+
+}
+
+function calcularresta(){
+    var num1, num2;
+    num1=Number(document.getElementsByName("res_num1")[0].value);
+    num2=Number(document.getElementsByName("res_num2")[0].value);
+
+    document.getElementsByName("res_total")[0].value = num1 - num2;
+
+}
+
+function calcularmultiplicacion(){
+    var num1, num2;
+    num1=Number(document.getElementsByName("mul_num1")[0].value);
+    num2=Number(document.getElementsByName("mul_num2")[0].value);
+
+    document.getElementsByName("mul_total")[0].value = num1 * num2;
+
+}
+
+function calculardivision(){
+    var num1, num2;
+    num1=Number(document.getElementsByName("div_num1")[0].value);
+    num2=Number(document.getElementsByName("div_num2")[0].value);
+
+    document.getElementsByName("div_total")[0].value = num1 / num2;
+
+}
