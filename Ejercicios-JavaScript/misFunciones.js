8ad608e3fcb27b9975b1025bb6f93c83140abc22
catalinarubies <2211430@ucc.edu.ar>
2024-06-08 20:26:27 -0300

    se utilizo inner.html

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -66,7 +66,7 @@ function calcularsuma(){
     num1=Number(document.getElementsByName("sum_num1")[0].value);
     num2=Number(document.getElementsByName("sum_num2")[0].value);
 
-    document.getElementsByName("sum_total")[0].value = num1 + num2;
+    document.getElementsByName("sum_total")[0].innerHTML = num1 + num2;
 
 }
 
@@ -75,7 +75,7 @@ function calcularresta(){
     num1=Number(document.getElementsByName("res_num1")[0].value);
     num2=Number(document.getElementsByName("res_num2")[0].value);
 
-    document.getElementsByName("res_total")[0].value = num1 - num2;
+    document.getElementsByName("res_total")[0].innerHTML = num1 - num2;
 
 }
 
@@ -84,7 +84,7 @@ function calcularmultiplicacion(){
     num1=Number(document.getElementsByName("mul_num1")[0].value);
     num2=Number(document.getElementsByName("mul_num2")[0].value);
 
-    document.getElementsByName("mul_total")[0].value = num1 * num2;
+    document.getElementsByName("mul_total")[0].innerHTML = num1 * num2;
 
 }
 
@@ -93,6 +93,6 @@ function calculardivision(){
     num1=Number(document.getElementsByName("div_num1")[0].value);
     num2=Number(document.getElementsByName("div_num2")[0].value);
 
-    document.getElementsByName("div_total")[0].value = num1 / num2;
+    document.getElementsByName("div_total")[0].innerHTML = num1 / num2;
 
 }
